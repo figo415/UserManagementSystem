2820157d869848dc073e71afcf3cdfe7273d5583
Dayong <ludayong20@gmail.com>
2021-06-20 22:03:08 -0400

    add pipeline config

---
+++ pipeline_config.groovy
@@ -0,0 +1,11 @@
+libraries {
+    dotnet
+}
+
+keywords {
+    imageName = 'mega-kunpeng-api'
+    argoAppName = 'mega-kunpeng-api'
+    imageTagFlag = 'image.tag'
+    projectName = 'lims'
+    releaseVersion = 'REL-1.0.0'
+}
